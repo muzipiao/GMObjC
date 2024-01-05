e325b6d4027a8901151022f039c74e59d011bac6
lifei <lifei_zdjl@126.com>
2024-01-05 18:41:54 +0800

    Mac Demo

--- Examples/GMObjC Mac Demo/GMMainVC.swift
+++ Examples/GMObjC Mac Demo/GMMainVC.swift
@@ -9,7 +9,9 @@ import Cocoa
 
 class GMMainVC: NSViewController {
     
+    private let kGMTestColumnID = NSUserInterfaceItemIdentifier("kGMTestColumnID")
     private let kGMTestCellID = NSUserInterfaceItemIdentifier("kGMTestCellID")
+    private var modelList: [GMTestModel] = []
     
     override func loadView() {
         super.loadView()
@@ -21,56 +23,104 @@ class GMMainVC: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(self.tableView)
-        print(self.scrollView)
-        print(self.view)
-        self.view.window?.delegate = self
         self.view.layer?.backgroundColor = NSColor.green.cgColor
+        // 运行示例
+        self.modelList.append(GMTestUtil.testSm2EnDe())
+        self.modelList.append(GMTestUtil.testSm2Sign())
+        self.modelList.append(GMTestUtil.testSm3())
+        self.modelList.append(GMTestUtil.testSm4())
+        self.modelList.append(GMTestUtil.testECDH())
+        self.modelList.append(GMTestUtil.testReadPemDerFiles())
+        self.modelList.append(GMTestUtil.testSaveToPemDerFiles())
+        self.modelList.append(GMTestUtil.testCreateKeyPairFiles())
+        self.modelList.append(GMTestUtil.testCompressPublicKey())
+        self.modelList.append(GMTestUtil.testConvertPemAndDer())
+        self.modelList.append(GMTestUtil.testReadX509FileInfo())
+        // 创建视图
+        self.tableView.addTableColumn(self.tableColumn)
+        self.scrollView.documentView = self.tableView
         self.view.addSubview(self.scrollView)
+        self.scrollView.frame = self.view.bounds
+        self.tableView.frame = self.view.bounds
+        self.tableColumn.width = self.view.bounds.size.width
         self.tableView.reloadData()
     }
     
     // MARK: - Lazy Load
     lazy var scrollView: NSScrollView = {
         let tmpView = NSScrollView(frame: self.view.bounds)
-        tmpView.documentView = self.tableView
         return tmpView
     }()
     
     lazy var tableView: NSTableView = {
         let tmpView = NSTableView(frame: self.view.bounds)
         if #available(macOS 11.0, *) {
-            tmpView.style = .fullWidth
+            tmpView.style = NSTableView.Style.automatic
         }
+        tmpView.autoresizingMask = NSView.AutoresizingMask.width
+        tmpView.rowSizeStyle = NSTableView.RowSizeStyle.custom
+        tmpView.usesAutomaticRowHeights = true
+        tmpView.rowHeight = 44
+        tmpView.selectionHighlightStyle = .regular
+        tmpView.allowsColumnResizing = false
+        tmpView.allowsColumnSelection = false
+        tmpView.allowsColumnReordering = false
+        tmpView.headerView = nil
         tmpView.dataSource = self
         tmpView.delegate = self
-//        let cellNibName = NSNib.Name(reflecting: GMTestCell.self)
-//        tmpView.register(NSNib(nibNamed: cellNibName, bundle: nil), forIdentifier: kGMTestCellID)
-//        tableView.register(GMTestCell.self, forIdentifier: NSUserInterfaceItemIdentifier("CustomCellIdentifier"))
         return tmpView
     }()
-
-}
-
-extension GMMainVC: NSWindowDelegate {
-    func windowDidResize(_ notification: Notification) {
-        print("windowDidResize===\(notification)")
-    }
     
-    func windowDidEndLiveResize(_ notification: Notification) {
-        print("windowDidEndLiveResize===\(notification)")
-    }
+    lazy var tableColumn: NSTableColumn = {
+        let tmpView = NSTableColumn(identifier: self.kGMTestColumnID)
+        tmpView.resizingMask = NSTableColumn.ResizingOptions.autoresizingMask
+        return tmpView
+    }()
+    
+    lazy var colorList: [NSColor] = {
+        let color0 = NSColor.white
+        let color1 = NSColor(red: 250.0/255.0, green: 249.0/255.0, blue: 222.0/255.0, alpha: 1.0)
+        let color2 = NSColor(red: 255.0/255.0, green: 242.0/255.0, blue: 226.0/255.0, alpha: 1.0)
+        let color3 = NSColor(red: 253.0/255.0, green: 230.0/255.0, blue: 224.0/255.0, alpha: 1.0)
+        let color4 = NSColor(red: 227.0/255.0, green: 237.0/255.0, blue: 205.0/255.0, alpha: 1.0)
+        let color5 = NSColor(red: 220.0/255.0, green: 226.0/255.0, blue: 241.0/255.0, alpha: 1.0)
+        let color6 = NSColor(red: 233.0/255.0, green: 235.0/255.0, blue: 254.0/255.0, alpha: 1.0)
+        let color7 = NSColor(red: 234.0/255.0, green: 234.0/255.0, blue: 239.0/255.0, alpha: 1.0)
+        let colors = [color0, color1, color2, color3, color4, color5, color6, color7]
+        return colors
+    }()
 }
 
 // MARK: - NSTableViewDelegate, NSTableViewDataSource
 extension GMMainVC: NSTableViewDelegate, NSTableViewDataSource {
+
     func numberOfRows(in tableView: NSTableView) -> Int {
-        return 10
+        return self.modelList[0].itemList.count
     }
     
+    func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
+//        NSTableCellView
+        return 88
+    }
+    
+    //Recalculate when the screen moves
+//    func tableViewColumnDidResize(_ notification: Notification) {
+//      let allIndex = IndexSet(integersIn:0..<self.YOURTABLE.numberOfRows)
+//     YOURTABLE.noteHeightOfRows(withIndexesChanged: allIndex)
+// }
+    
+//    func tableViewColumnDidResize(_ notification: Notification) {
+//        let allIndex = IndexSet(integersIn: 0..<self.modelList[0].itemList.count)
+//        self.tableView.noteHeightOfRows(withIndexesChanged: allIndex)
+//    }
+    
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-        let cell = tableView.makeView(withIdentifier: kGMTestCellID, owner: self)
-        cell?.layer?.backgroundColor = NSColor.orange.cgColor
+        var cell = tableView.makeView(withIdentifier: self.kGMTestCellID, owner: self) as? GMTestCell
+        if (cell == nil) {
+            cell = GMTestCell(frame: NSRect.zero)
+        }
+        guard let model = self.modelList[0].itemList[row] as? GMTestItemModel else { return cell }
+        cell?.titleLabel.string = model.detail
         return cell
     }
 }
---
+++ Examples/GMObjC Mac Demo/GMObjC_Mac_Demo-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  GMObjC_Mac_Demo-Bridging-Header.h
+//  GMObjCDemo
+//
+//  Created by lifei on 2024/1/5.
+//
+
+#ifndef GMObjC_Mac_Demo_Bridging_Header_h
+#define GMObjC_Mac_Demo_Bridging_Header_h
+
+#import "GMTestModel.h"
+#import "GMTestUtil.h"
+
+#endif /* GMObjC_Mac_Demo_Bridging_Header_h */
